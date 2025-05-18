5d72938659bb59dfc60d77fb8a4cdaf4207b3e85
philipreese <pbreese42@gmail.com>
2025-05-17 22:38:46 -0400

    add help and exit commands

--- src/repl.ts
+++ src/repl.ts
@@ -1,5 +1,23 @@
 import { createInterface } from 'node:readline';
 import { stdin, stdout } from 'node:process';
+import { commandExit } from './command_exit.js';
+import { commandHelp } from './command_help.js';
+import { CLICommand } from './command.js';
+
+export function getCommands(): Record<string, CLICommand> {
+    return {
+        help: {
+            name: "help",
+            description: "Displays a help message",
+            callback: commandHelp,
+        },
+        exit: {
+            name: "exit",
+            description: "Exits the pokedex",
+            callback: commandExit,
+        },
+    }
+}
 
 export function startREPL() {
     const rl = createInterface({
@@ -13,7 +31,13 @@ export function startREPL() {
     rl.on('line', (input) => {
         const words = cleanInput(input);
         if (words.length > 0) {
-            console.log(`Your command was: ${words[0]}`);
+            const commandWord = words[0];
+            const commands = getCommands();
+            if (commandWord in commands) {
+                commands[commandWord].callback(commands);
+            } else {
+                console.log("Unknown command")
+            }
         }
         rl.prompt();
     }); 
