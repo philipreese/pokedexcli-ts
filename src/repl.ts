d8dd923b0d76e55b519bb6cd599951dc9a2bf5a9
philipreese <pbreese42@gmail.com>
2025-05-17 21:04:15 -0400

    create simple repl

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,24 @@
+import { createInterface } from 'node:readline';
+import { stdin, stdout } from 'node:process';
+
+export function startREPL() {
+    const rl = createInterface({
+        input: stdin,
+        output: stdout,
+        prompt: "Pokedex > "
+    });
+
+    rl.prompt();
+
+    rl.on('line', (input) => {
+        const words = cleanInput(input);
+        if (words.length > 0) {
+            console.log(`Your command was: ${words[0]}`);
+        }
+        rl.prompt();
+    }); 
+}
+
 export function cleanInput(input: string): string[] {
     return input.trim().toLowerCase().split(" ").filter((i) => i !== "");
 }
